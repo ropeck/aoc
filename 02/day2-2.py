3a48b9fd0ae5e15790961858fd7e6bdfa8bdc1b8
Rodney Peck <ropeck@google.com>
2022-12-01 22:08:46 -0800

    checkin day2

--- 02/day2-2.py
+++ 02/day2-2.py
@@ -4,21 +4,29 @@
 # Rock A/X, Paper B/Y, Scissor C/Z
 # score X:1, Y:2, Z:3
 #
-# A Z
+# X lose, Y draw, Z win
+#
+score = {"A": {"A":3, "B":6, "C":0},
+         "B": {"A":0, "B":3, "C":6},
+         "C": {"A":6, "B":0, "C":3}}
 
-score = {"A": {"X":3, "Y":6, "Z":0},
-         "B": {"X":0, "Y":3, "Z":6},
-         "C": {"X":6, "Y":0, "Z":3}}
+init = {"A": 1, "B": 2, "C": 3}
 
-init = {"X": 1, "Y": 2, "Z": 3}
-
+def st(opp, out):
+  if out == "Y":
+    return opp
+  return {"A": {"X": "C", "Z": "B"},
+          "B": {"X": "A", "Z": "C"},
+          "C": {"X": "B", "Z": "A"}}[opp][out]
 
 def main():
   total = 0 
   for line in open("input","r"):
-    (opp, you) = line.split()
+    print line
+    (opp, out) = line.split()
+    you = st(opp, out)
     game = score[opp][you] + init[you]
-    print opp, you, game
+    print game
     total += game
   print("total: ", total)
 if __name__ == '__main__':
