a4312314be3af8d185e5fbb94f74dc3e42382e49
Rodney Peck <ropeck@google.com>
2022-12-05 21:13:31 -0800

    day6 step1 done

--- 06/step1.py
+++ 06/step1.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def main():
+  MARKER_LEN=4
   with open("input","r") as fh:
     buffer = fh.read()
   while True:
-    for i in range(4, len(buffer)):
-      marker = buffer[i-4:i]
-      if len(set(marker)) == 4:
+    for i in range(MARKER_LEN, len(buffer)):
+      marker = buffer[i-MARKER_LEN:i]
+      if len(set(marker)) == MARKER_LEN:
         print(f'{i} {marker}')
         exit(0)
 if __name__ == '__main__':
--- 06/step2.py
+++ 06/step2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
-
-
 def main():
-  total = 0
-  for line in open("input","r"):
-
-  print(f'total: {total}')
+  MARKER_LEN=14
+  with open("input","r") as fh:
+    buffer = fh.read()
+  while True:
+    for i in range(MARKER_LEN, len(buffer)):
+      marker = buffer[i-MARKER_LEN:i]
+      if len(set(marker)) == MARKER_LEN:
+        print(f'{i} {marker}')
+        exit(0)
 if __name__ == '__main__':
   main()
