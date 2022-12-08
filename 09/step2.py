290f843c59575b2f275bad240b223f0fe28f5411
Rodney Peck <ropeck@google.com>
2022-12-08 11:15:28 -0800

    setup day 09

---
+++ 09/step2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def main():
+  total = 0
+  # with open("input","r") as fh:
+  #   line = fh.readline().strip()
+  #   while True:
+
+  for line in open("input","r"):
+
+  print(f'total: {total}')
+if __name__ == '__main__':
+  main()
