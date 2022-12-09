0dffc809def9f52a6a0f6b35df9b5c002fad323e
Rodney Peck <ropeck@google.com>
2022-12-08 22:34:35 -0800

    working on day 9

--- 09/step1.py
+++ 09/step1.py
@@ -1,13 +1,50 @@
 #!/usr/bin/python3
+import sys
 
-def main():
-  total = 0
-  # with open("input","r") as fh:
-  #   line = fh.readline().strip()
-  #   while True:
+def main(path):
+  total = []
+  head = [0,0]
+  tail = head.copy()
+  for line in open(path,"r"):
+    if tail.copy() not in total:
+      total.append(tail.copy())
+    (m, n) = line.strip().split()
+    print(m, n)
+    for i in range(int(n)):
+      print(f'h:{head} t:{tail}')
+      match m:
+        case 'U':
+          head[1] += 1
+        case 'D':
+          head[1] -= 1
+        case 'L':
+          head[0] -= 1
+        case 'R':
+          head[0] += 1
+      if head == tail:
+        continue
+      if head[1] == tail[1]:
+        d = head[0] - tail[0]
+        if abs(d) > 1:
+          tail[0] += int(d/abs(d))
+          if tail[1] != head[1]:
+            tail[1] = head[1]
+        continue
+      if head[0] == tail[0]:
+        d = head[1] - tail[1]
+        if abs(d) > 1:
+          tail[1] += int(d/abs(d))
+          if tail[0] != head[0]:
+            tail[0] = head[0]
+        continue
 
-  for line in open("input","r"):
 
-  print(f'total: {total}')
+
+  print(f'total: {len(total)}')
+
 if __name__ == '__main__':
-  main()
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+  else:
+    path = "input"
+  main(path)
