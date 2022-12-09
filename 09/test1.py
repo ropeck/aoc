0354feab0a54deb55d4a350561b49e40e873303b
Rodney Peck <ropeck@google.com>
2022-12-08 23:11:44 -0800

    added tests

--- 09/test1.py
+++ 09/test1.py
@@ -5,4 +5,4 @@ import step1
 
 class TestStep1TestInputLength(unittest.TestCase):
     def runTest(self):
-        self.assertEqual(len(step1.main("test_input")), 21)
+        self.assertEqual(len(step1.main("test_input")), 13)
