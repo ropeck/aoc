3589bcce46a2f5c3d6810f7a27c1f2cb705b66b9
Rodney Peck <ropeck@google.com>
2022-12-09 20:48:39 -0800

    debugging

--- 09/step1.py
+++ 09/step1.py
@@ -1,24 +1,46 @@
 #!/usr/bin/python3
+import math
 import sys
-from math import sqrt
+from math import sqrt, copysign
+
+def move(h, t):
+  dx = h[0] - t[0]
+  dy = h[1] - t[1]
+  d = sqrt(dx ** 2 + dy ** 2)
+  if d > sqrt(2):
+    if dx:
+      t[0] += int(math.copysign(1, dx))
+    if dy:
+      t[1] += int(math.copysign(1, dy))
+  return t
+
+def draw(r, t):
+  w = len(t)
+  h = len(eval(t[0]))
+  b = [[' ' for x in range(w)] for y in range(h)]
+  for s in t:
+    (x,y) = eval(s)
+    y = h - y -1
+    b[x][y] = '#'
+  b[r[0][0]][h - r[0][1] -1] = "H"
+  b[r[1][0]][h - r[1][1] -1] = "T"
+  for l in b:
+    print(''.join(l))
+
 
 def main(path):
-  total = []
-  head = [0,0]
-  tail = head.copy()
+  rope = []
+  rope.append([0,0])
+  rope.append([0,0])
+  total = [str(rope[-1])]
+
   with open(path, "r") as fh:
     step_data = fh.read()
   for line in step_data.splitlines():
-    if tail not in total:
-      total.append(tail.copy())
     (m, n) = line.strip().split()
     print(m, n)
     for i in range(int(n)):
-      if tail not in total:
-        total.append(tail.copy())
-      d = sqrt(abs(head[0]-tail[0])**2 + abs(head[1]-tail[1])**2)
-      if d > sqrt(2):
-        tail = head.copy()
+      head = rope[0]
       match m:
         case 'U':
           head[1] += 1
@@ -28,9 +50,16 @@ def main(path):
           head[0] -= 1
         case 'R':
           head[0] += 1
+      rope[0] = head
+      tail = move(rope[0], rope[1])
+      rope[1] = tail
+      total.append(str(rope[-1]))
       print(f'   h:{head} t:{tail}')
-  print(f'total: {len(total)}')
-  return(total)
+      draw(rope, total)
+  print(set(total))
+  path_len = len(set(total))
+  print(f'total: {path_len}')
+  return(path_len)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
--- 09/test1.py
+++ 09/test1.py
@@ -2,7 +2,6 @@
 import unittest
 import step1
 
-
 class TestStep1TestInputLength(unittest.TestCase):
     def runTest(self):
-        self.assertEqual(len(step1.main("test_input")), 13)
+        self.assertEqual(13, step1.main("test_input"))
