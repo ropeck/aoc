ad46d46f87610fd593bc258476ab8ce5e43b3943
Rodney Peck <ropeck@google.com>
2022-12-11 22:26:05 -0800

    working 12.1

--- 12/step1.py
+++ 12/step1.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
 import sys
 
-def main(path):
-  total = 0
-  # with open("input","r") as fh:
-  #   line = fh.readline().strip()
-  #   while True:
+class Map:
+  def __init__(self, path):
+    m = []
+    with open(path, "r") as fh:
+      for l in fh:
+        m.append([ord(i)-ord('a') for i in l.strip()])
+    self.map = m
 
-  for line in open(path,"r"):
-    pass
-  print(f'total: {total}')
+  def print(self):
+    for l in self.map:
+      print(l)
+    for o in "start finish width height".split():
+      print(f'{o}: {eval("self."+o+"()")}')
+
+  def find_char(self, char):
+    y = 0
+    for l in self.map:
+      x = 0
+      for i in l:
+        if self.map[y][x] == ord(char)-ord('a'):
+          return (x, y)
+        x += 1
+      y += 1
+
+  def start(self):
+    return self.find_char('S')
+
+  def finish(self):
+    return self.find_char('E')
+
+  def width(self):
+    return len(self.map[0])
+
+  def height(self):
+    return len(self.map)
+
+def main(path):
+  map = Map(path)
+  map.print()
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
