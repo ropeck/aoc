d15418ba7a7c96ab0a14b4493413956685fda574
Rodney Peck <ropeck@google.com>
2022-12-11 21:27:46 -0800

    working on 11.2

--- 11/step2.py
+++ 11/step2.py
@@ -1,92 +1,159 @@
 #!/usr/bin/python3
-import math
+import re
 import sys
-
-ROOT2 = math.sqrt(2)
 
 def debug(str):
   # debug(str)
   pass
-def span(t, r, n):
 
- low = min([x[n] for x in r] + [eval(x)[n] for x in t])
- high = max([x[n] for x in r] + [eval(x)[n] for x in t])
- return high - low
+class Monkey():
+  """
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+  """
+  def __init__(self):
+    self.number = None
+    self.items = []
+    self.operation = None
+    self.test_str = None
+    self.if_true = None
+    self.if_false = None
+    self.inspections = 0
+  def inspect(self):
+    self.inspections += 1
 
-def draw(r, t):
-  def mark(n, m):
-    y = h - r[n][1] - 1 + ymin
-    x = r[n][0] - xmin
-    b[y][x] = m
+  def item_list_str(self):
+    return ", ".join([str(x) for x in self.items])
 
-  tx=[]
-  ty=[]
-  for s in r:
-    tx.append(s[0])
-    ty.append(s[1])
-  for s in t:
-    (x,y) = eval(s)
-    tx.append(x)
-    ty.append(y)
-  xmin = min(tx)
-  ymin = min(ty)
-  w = span(t, r, 0)+1-xmin
-  h = span(t, r, 1)+1-ymin
-  b = [['.' for x in range(w)] for y in range(h)]
-  for s in t:
-    (x, y) = eval(s)
-    a = h-1-y+ymin
-    b2 = x-xmin
-    b[h-1-y+ymin][x-xmin] = '#'
+  def inspection_info(self):
+    return f'Monkey {self.number} inspected items {self.inspections} times.'
 
-  mark(0, "H")
-  mark(1, "T")
-  for l in b:
-    debug(''.join(l))
+  def __str__(self):
+    return """Monkey {}:
+  Starting items: {}
+  Operation: new = old {}
+  Test: {}
+    If true: throw to monkey {}
+    If false: throw to monkey {}""".format(
+      self.number, self.item_list_str(), self.operation, self.test_str, self.if_true, self.if_false)
 
-def move_tail(h, t):
-  d = [h[i] - t[i] for i in range(len(h))]
-  if math.sqrt(sum([n**2 for n in d])) > ROOT2:
-    for n in range(len(h)):
-      if d[n]:
-        t[n] += int(math.copysign(1, d[n]))
-  return t
+  def __repr__(self):
+    return "<Monkey {} [{}]>".format(self.number, self.item_list_str())
+
+  def parse_fh(fh):
+    def parse_line(label="", regexp=None):
+      l = fh.readline().strip()
+      if not l:
+        return None
+      if not regexp:
+        regexp = ':\s?(.*)'
+      expanded_regexp = label + regexp
+      m = re.match(expanded_regexp, l)
+      if not m:
+        raise ValueError("expecting '{}' but found '{}'".format(expanded_regexp, l))
+      return m.group(1)
+    def parse_if_line(cond):
+      return int(parse_line("If {}: throw to monkey".format({True: "true", False: "false"}[cond]), regexp=" (.*)"))
+    def parse_items_line():
+      return [int(i) for i in re.split(",\s", parse_line("Starting items"))]
+
+    monkey = parse_line("Monkey", regexp=" (.*):")
+    if monkey == None:
+      return None
+    m = Monkey()
+    m.number = int(monkey)
+    m.items = parse_items_line()
+    m.operation = parse_line("Operation: new = old", regexp=" (.*)")
+    (m.op_cmd, m.op_val) = m.operation.split()
+    if m.op_cmd not in "+*":
+      raise ValueError(f'{self.op_cmd} operation not found')
+    if m.op_val != "old":
+      m.op_val = int(m.op_val)
+    m.test_str = parse_line("Test")
+    mm = re.match("divisible by (.*)", m.test_str)
+    m.test_divisible = int(mm.group(1))
+    m.if_true = parse_if_line(True)
+    m.if_false = parse_if_line(False)
+    l = fh.readline()
+    return m
+
+  def operate(self, old):
+    if self.op_val == "old":
+      n = old
+    else:
+      n = self.op_val
+    if self.op_cmd == "+":
+      return n + old
+    if self.op_cmd == "*":
+#      if n == old:
+#        return n
+      return n * old
+
+  def next_monkey(self, old):
+    if old % self.test_divisible == 0:
+            return self.if_true
+    else:
+      return self.if_false
+
+  def append_item(self, i):
+    self.items.append(i)
+    print(f'{self.number} append ')
+
+def prime_reduce(n):
+  r = 1
+  for p in [2, 3, 5, 7, 11, 13, 17, 19]:
+    if n % p == 0:
+      r = r * p
+  return r
 
 def main(path):
-  rope = [[0,0] for i in range(2)]
-
-  total = []
-
+  monkey = []
   with open(path, "r") as fh:
-    step_data = fh.read()
-  for line in step_data.splitlines():
-    (m, n) = line.strip().split()
-    debug(f'== {m} {n} ==')
-    debug('')
-    for i in range(int(n)):
-      head = rope[0]
-      match m:
-        case 'U':
-          head[1] += 1
-        case 'D':
-          head[1] -= 1
-        case 'L':
-          head[0] -= 1
-        case 'R':
-          head[0] += 1
-      rope[0] = head
-      for i in range(len(rope)-1):
-        tail = move_tail(rope[i], rope[i+1])
-        rope[i+1] = tail
-      total.append(str(tail))
-      debug(total)
-      # debug(f'   h:{head} t:{tail}')
-      # draw(rope, total)
-      debug('')
-  debug(set(total))
-  path_len = len(set(total))
-  print(f'total: {path_len}')
-  return(path_len)
+    while True:
+      m = Monkey.parse_fh(fh)
+      if m:
+        monkey.append(m)
+      else:
+        break
+
+  round = 0
+  #while round < 10000:
+  while round < 20:
+    round += 1
+    print(f'== ROUND {round} ==')
+    for m in monkey:
+      items = m.items
+      m.items = []
+      for i in items:
+        m.inspect()
+        print('')
+    #    print(f'{m.number} item {i}')
+        new = m.operate(i)
+        new = prime_reduce(new)
+        next = m.next_monkey(new)
+        monkey[next].append_item(new)
+    print('')
+
+    if round % 1000 == 0:
+      print(f'\n== After round {round} ==')
+      for m in monkey:
+        print(m.inspection_info())
+  print(f'\n== After round {round} ==')
+  for m in monkey:
+    print(m.inspection_info())
+  mb = 1
+
+  insp = [i.inspections for i in monkey]
+  insp.sort()
+
+  for i in insp[-2:]:
+    print(f'{mb} insp {i}')
+    mb = mb * i
+  print(f'monkey business: {mb}')
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
