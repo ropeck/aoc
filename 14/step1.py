f1f1e4e3e2d69a20e0639b8edb49b319bf95b565
Rodney Peck <ropeck@google.com>
2022-12-13 23:09:52 -0800

    day 14 init

--- 14/step1.py
+++ 14/step1.py
@@ -7,7 +7,7 @@ class Drawing:
     drawing = []
     with open(path,"r") as fh:
       for line in fh:
-        drawing.append([tuple(p.split(",")) for p in line.strip().split(" -> ")])
+        drawing.append([tuple([int(x) for x in p.split(",")]) for p in line.strip().split(" -> ")])
     for l in drawing:
       print(l)
     self.d = drawing
@@ -16,10 +16,35 @@ class Drawing:
     self.max_x = max([x for (x,y) in flat])
     self.min_y = min([y for (x,y) in flat])
     self.max_y = max([y for (x,y) in flat])
+    self.width = self.max_x - self.min_x
+    self.height = self.max_y - self.min_y
+
+    self.grid = [[" " for x in range(self.width)] for y in range(self.height)]
+
+    for l in drawing:
+      c = l[-1]
+      for m in l[:-1]:
+        if c[0] == m[0]:
+          # vertical line
+          vl = [m[1], c[1]]
+          vl.sort()
+          for y in range(vl[0], vl[1]):
+            self.grid[y-self.min_y-1][c[0]-self.min_x-1] = '#'
+        else:
+          # horizontal line
+          hl = [m[0], c[0]]
+          hl.sort()
+          for x in range(hl[0], hl[1]):
+            self.grid[c[1]-self.min_y-1][x-self.min_x-1] = '#'
+    self.print()
+
+  def print(self):
+    print("\n".join(["".join(l) for l in self.grid]))
 
 
 def main(path):
   d = Drawing(path)
+  # d.print()
   return d.d
 
 if __name__ == '__main__':
--- 14/test/test1.py
+++ 14/test/test1.py
@@ -4,8 +4,8 @@ import step1
 
 class TestStep1TestInputLength(unittest.TestCase):
     def runTest(self):
-        self.assertEqual([[('498', '4'), ('498', '6'), ('496', '6')],
-                          [('503', '4'), ('502', '4'), ('502', '9'), ('494', '9')]],
+        self.assertEqual([[(498, 4), (498, 6), (496, 6)],
+                          [(503, 4), (502, 4), (502, 9), (494, 9)]],
                          step1.Drawing("test/test_input").d)
 
 #498,4 -> 498,6 -> 496,6
