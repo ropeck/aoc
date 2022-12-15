13c5850d9f149305b788e0e01566b28791df1d34
Rodney Peck <ropeck@google.com>
2022-12-14 23:07:45 -0800

    looking at day15 distance eq

--- 15/step1.py
+++ 15/step1.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 import sys
 
+# parse beacons and sensors
+# Sensor at x=3558476, y=2123614: closest beacon is at x=4305648, y=2127118
+
+# find intersection with line at y=20000
+class Beacon:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def distance(self, other):
+    return abs(self.x - other.x) + abs(self.y - other.y)
+  def intersecting_slice(self, y):
+    # somehow find the start and end of the part that is within range
+
 def main(path):
   total = 0
   # with open("input","r") as fh:
