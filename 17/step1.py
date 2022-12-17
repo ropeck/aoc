a6bd2618f214029c1aecdd88bc00b7371266b2a1
Rodney Peck <ropeck@google.com>
2022-12-17 00:02:48 -0800

    working

--- 17/step1.py
+++ 17/step1.py
@@ -32,9 +32,16 @@ class Tower:
     # self.t + [0, 0, 0]
     # loop from top down, check to see if the rock overlaps
     # use binary AND of the tower with the rock - if (rock & tower top ) != 0 then it's colliding
-    tower = self.t + [0, 0, 0] + [0 for i in len(r)]
+    tower = self.t + [0, 0, 0] + [0 for i in r[1]]
     tower.reverse()
-    while True:
+    for i, row in enumerate(tower):
+      if row & r[1][0]:
+        break
+    tower[i-1] = tower[i-1] | r[1][0]
+    tower.reverse()
+    self.t = [row for row in tower if row]
+    print(self.t)
+    # handle multiple line rocks
       # if overlap(tower, r, i):
       #   mark r
       #   break
@@ -58,9 +65,13 @@ class Tower:
 
 def main(path):
   t = Tower()
+  t.drop(t.next_rock())
+  t.drop(t.next_rock())
+
   for i in range(10):
     print(t.next_rock())
     print("")
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
