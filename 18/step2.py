b45fdd4fc684ef66ea18890441fe085843fc18a8
Rodney Peck <ropeck@google.com>
2022-12-17 23:12:28 -0800

    first star 18

--- 18/step2.py
+++ 18/step2.py
@@ -23,6 +23,7 @@ def main(path):
             pass
           else:
             if (x+xd, y+yd, z+zd) not in p:
+              c[(x+xd,y+yd,z+zd)] = -1
               s += 1
             else:
               c[(x+xd,y+yd,z+zd)] = c.get((x+xd,y+yd,z+zd),0) +1
@@ -30,10 +31,27 @@ def main(path):
       if s == 0:
         s == -1
     print(f'({x}, {y}, {z}) {s}')
-    total += s 
+    total += s
 
   print(f'total: {total}')
   pprint(c)
+
+  for q in c:
+    if c[q] > 0:
+      continue
+    s = 0
+    (x,y,z) = q
+    for xd in range(-1, 2):
+      for yd in range(-1, 2):
+        for zd in range(-1, 2):
+          if ((sum([abs(n) for n in [xd, yd, zd]]) > 1)):
+            continue
+          if (x+xd, y+yd, z+zd) in p:
+            s += 1
+    if s == 6:
+      print(f'{q} {s}')
+      total -= s
+  print(f'total: {total}')
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
