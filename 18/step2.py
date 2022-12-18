1b20f0980b59579f0639b30f1774fc6c16b08919
Rodney Peck <ropeck@google.com>
2022-12-17 23:56:28 -0800

    flailing for second star 18

--- 18/step2.py
+++ 18/step2.py
@@ -3,15 +3,8 @@ import numpy as np
 from pprint import pprint
 import sys
 
-
-def main(path):
-  p = []
+def count_faces(p):
   c = {}
-  with open(path, "r") as fh:
-    for l in fh:
-      print(l.strip())
-      p.append(tuple([int(x) for x in l.strip().split(",")]))
-
   total = 0
   for (x, y, z) in p:
     s = 0
@@ -32,26 +25,52 @@ def main(path):
         s == -1
     print(f'({x}, {y}, {z}) {s}')
     total += s
+  return total, c
 
+def is_inside(q, p):
+  (x,y,z) = q
+  xx = [x1 for (x1,y1,z1) in p if z==z1 and y==y1]
+  if len(xx) < 2 or len(xx) % 2:
+    return False
+  xx.sort()
+  if xx[0] > x or xx[-1] < x:
+    return False
+  yy = [y1 for (x1,y1,z1) in p if x==x1 and z==z1]
+  if len(yy) < 2 or len(yy) % 2:
+    return False
+  yy.sort()
+  if yy[0] > y or yy[-1] < y:
+    return False
+  zz = [z1 for (x1,y1,z1) in p if x==x1 and y==y1]
+  zz.sort()
+  if len(zz) < 2 or len(zz) % 2:
+    return False
+  if zz[0] > z or zz[-1] < z:
+    return False
+  print(f'is inside {q}')
+  return True
+
+def main(path):
+  p = []
+  with open(path, "r") as fh:
+    for l in fh:
+      print(l.strip())
+      p.append(tuple([int(x) for x in l.strip().split(",")]))
+
+  total, c = count_faces(p)
   print(f'total: {total}')
   pprint(c)
 
+  inside = []
   for q in c:
     if c[q] > 0:
       continue
-    s = 0
-    (x,y,z) = q
-    for xd in range(-1, 2):
-      for yd in range(-1, 2):
-        for zd in range(-1, 2):
-          if ((sum([abs(n) for n in [xd, yd, zd]]) > 1)):
-            continue
-          if (x+xd, y+yd, z+zd) in p:
-            s += 1
-    if s == 6:
-      print(f'{q} {s}')
-      total -= s
-  print(f'total: {total}')
+    if is_inside(q, p):
+      inside.append(q)
+  t, cc = count_faces(inside)
+  print(f'int {t} {inside}')
+  print(f'total: {total-t}')
+  print(f'total: {total-6*len(inside)}')
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
