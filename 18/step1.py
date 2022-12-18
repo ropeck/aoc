b4ee15216c1229a290f74a5932b614cc2fb68264
Rodney Peck <ropeck@google.com>
2022-12-17 22:48:26 -0800

    first star 18

--- 18/step1.py
+++ 18/step1.py
@@ -10,10 +10,24 @@ def main(path):
     for l in fh:
       print(l.strip())
       p.append(tuple([int(x) for x in l.strip().split(",")]))
-  pprint(p)
-  sort(p)
-  pprint(p)
 
+  total = 0
+  for (x, y, z) in p:
+    s = 0
+    for xd in range(-1, 2):
+      for yd in range(-1, 2):
+        for zd in range(-1, 2):
+          if ((sum([abs(n) for n in [xd, yd, zd]]) > 1)):
+            #print(f'big  {xd} {yd} {zd}  ({x+xd}, {y+yd}, {z+zd})')
+            pass
+          else:
+            if (x+xd, y+yd, z+zd) not in p:
+              s += 1
+            else:
+              print(f'  {xd} {yd} {zd}  ({x+xd}, {y+yd}, {z+zd}) {s}')
+    print(f'({x}, {y}, {z}) {s}')
+    total += s 
+  print(f'total: {total}')
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
