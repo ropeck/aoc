fd89fefaab69b568e9693b0cf8d50e44263877b8
Rodney Peck <ropeck@google.com>
2022-12-19 12:33:20 -0800

    map items to code

--- 19/step1.py
+++ 19/step1.py
@@ -2,8 +2,17 @@
 import re
 import sys
 
+ORE=0
+CLAY=1
+OBSIDIAN=2
+GEODE=3
+
 class Blueprint:
   def __init__(self, line):
+    self._name={'ore': ORE, 'clay': CLAY, 'obsidian': OBSIDIAN, 'geode': GEODE}
+    self._item={}
+    for k,v in self._name.items():
+      self._item[v] = k
     line = line.strip()
     line = line.replace(".","")
     p = line.split("Each ")
@@ -15,12 +24,15 @@ class Blueprint:
       if not m:
         print(e)
         exit(1)
-      robot = m.group(1)
+      robot = self._name[m.group(1)]
       cost = {}
       for item in m.group(2).strip().split(" and "):
         (count, name) = item.split(" ")
+        name = self._name[name]
         cost[name] = int(count)
       self.bp[robot] = cost
+
+
   def __repr__(self):
     return f'<Blueprint {self.number} {self.bp}>'
 
