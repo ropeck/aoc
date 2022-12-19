2eb4c3669393515fdc8d67014c51c40546a21660
Rodney Peck <ropeck@google.com>
2022-12-18 21:55:51 -0800

    day 19

---
+++ 19/step1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import numpy as np
+from pprint import pprint
+import sys
+
+
+def main(path):
+  p = []
+  with open(path, "r") as fh:
+    for l in fh:
+      print(l.strip())
+      p.append(tuple([int(x) for x in l.strip().split(",")]))
+
+  total = 0
+  for (x, y, z) in p:
+    s = 0
+    for xd in range(-1, 2):
+      for yd in range(-1, 2):
+        for zd in range(-1, 2):
+          if ((sum([abs(n) for n in [xd, yd, zd]]) > 1)):
+            #print(f'big  {xd} {yd} {zd}  ({x+xd}, {y+yd}, {z+zd})')
+            pass
+          else:
+            if (x+xd, y+yd, z+zd) not in p:
+              s += 1
+            else:
+              print(f'  {xd} {yd} {zd}  ({x+xd}, {y+yd}, {z+zd}) {s}')
+    print(f'({x}, {y}, {z}) {s}')
+    total += s 
+  print(f'total: {total}')
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+  else:
+    path = "input"
+  main(path)
---
+++ 19/test/test1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import unittest
+import step1
+
+class TestStep1(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(64, step1.main("test/input"))
