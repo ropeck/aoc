592c268dbfce5356778efe50ca3c176970490cce
Rodney Peck <ropeck@google.com>
2022-12-20 10:52:46 -0800

    better

---
+++ 20/test/test2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import unittest
+import step2
+
+class TestStep1_1(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(48, step2.main("test/input"))
+
+class TestStep1_1mod(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(48, step2.main("test/input", True))
+
+class TestStep1mod(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(3760092545849, step2.main("input", True))
+
+class TestStep1(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(3760092545849, step2.main("input"))
+
