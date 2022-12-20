818ca5ed2cc76f7b56517a64904263a3a1d5a693
Rodney Peck <ropeck@google.com>
2022-12-19 23:19:26 -0800

    two stars on day 20!

---
+++ 20/step2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys
+
+def main(path):
+  d = []
+  with open(path, "r") as fh:
+    for line in fh:
+      d.append(int(line) * 811589153)
+
+  d = list(enumerate(d))
+  item_order = d.copy()
+  for mix in range(10):
+    for n in item_order:
+      i = d.index(n)
+      d.pop(i)
+      d.insert((i+n[1])%len(d), n)
+      #print(n, d)
+
+  for x in d:
+    if x[1] == 0:
+      i = x
+      break
+
+  res = [d[(n + d.index(x)) % len(d)][1] for n in [1000, 2000, 3000]]
+  print(res)
+  print(sum(res))
+
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+  else:
+    path = "input"
+  main(path)
