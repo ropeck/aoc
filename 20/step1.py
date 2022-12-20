c06183bdaa3b00403e7114669424203ce6b5c55a
Rodney Peck <ropeck@google.com>
2022-12-19 22:22:25 -0800

    DAY 20

--- 20/step1.py
+++ 20/step1.py
@@ -5,9 +5,29 @@ def main(path):
   d = []
   with open(path, "r") as fh:
     for line in fh:
-      d.append(int(fh.readline()))
+      d.append(int(line))
 
   print(d)
+  for n in d.copy():
+    if n == 0:
+      print(n, d)
+      continue
+    i = d.index(n)
+    d.pop(i)
+    if i+n == 0:
+      d.append(n)
+    else:
+      d.insert((i+n)%len(d), n)
+    print(n, d)
+
+  i = d.index(0)
+  l = len(d)
+  o = l - i
+
+  res = [d[((n-o)%l)] for n in [1000, 2000, 3000]]
+  print(res)
+  print(sum(res))
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
