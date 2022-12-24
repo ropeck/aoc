d014f090176ce6e7550bd16f140120ae3e85c497
Rodney Peck <ropeck@google.com>
2022-12-23 22:37:04 -0800

    working on code

--- 24/step1.py
+++ 24/step1.py
@@ -7,24 +7,33 @@ SPACE = "."
 STORM = {"<": (-1,0), ">": (1,0), "^": (0,-1), "v": (0,1)}
 
 class Storm:
-  def __init__(self, y, x, dir):
+  def __init__(self, board, x, y, dir):
     self.x = x
     self.y = y
     self.dir = dir
+    self.board = board
 
   def move(self):
     (dy, dx) = STORM[self.dir]
-    # check for wall at new spot and restart
+    ny = (self.y + dy) % self.board.height
+    nx = (self.x + dx) % self.board.width
+    while self.board.board[ny][nx] == WALL:
+      ny = (ny + dy) % self.board.height
+      nx = (nx + dx) % self.board.width
+    self.board.board[self.y][self.x] = SPACE
+    self.y = ny
+    self.x = nx
+    self.mark()
 
-  def mark(self, b):
-    v = b.b[self.y][self.x]
+  def mark(self):
+    v = self.board.board[self.y][self.x]
     if v in STORM.keys():
       n = "2"
-    if v != SPACE:
+    elif v != SPACE:
       n = str(int(v) + 1)
     else:
       n = self.dir
-    b.b[self.y][self.x] = n
+    self.board.board[self.y][self.x] = n
 
 class Valley:
   def __init__(self, path=None):
@@ -46,9 +55,16 @@ class Valley:
         for x, mark in enumerate(list(line.strip())):
           st = STORM.get(mark, False)
           if st:
-            self.storms.append(Storm(y, x, mark))
+            self.storms.append(Storm(self, x, y, mark))
         self.board.append(list(line.strip()))
         y += 1
+    self.height = len(self.board)
+    self.width = len(self.board[0])
+
+  def draw(self):
+    for l in self.board:
+      print("".join(l))
+    print("")
 
   def move_storms(self):
     for s in self.storms:
@@ -56,15 +72,17 @@ class Valley:
 
   def find_path(self):
     # put start on queue
-    while not queue_empty:
+    q = [(1,1)]
+    while q:
+      self.draw()
       self.move_storms()
-      nx, ny = pop_move
+      # nx, ny = pop_move
       # find possible moves, push them onto the queue to check. save storms too?
       # for each move:
       #   at exit?:
       #      add path score
-   sort the paths by score
-   return best path
+   # sort the paths by score
+   # return best path
 
 def main(path):
   v = Valley(path)
