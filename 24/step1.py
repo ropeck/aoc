539ef4273c3cfb3c0c010cc2c4ef604fee9090a5
Rodney Peck <ropeck@google.com>
2022-12-23 22:08:57 -0800

    working on code

--- 24/step1.py
+++ 24/step1.py
@@ -6,6 +6,26 @@ WALL = "#"
 SPACE = "."
 STORM = {"<": (-1,0), ">": (1,0), "^": (0,-1), "v": (0,1)}
 
+class Storm:
+  def __init__(self, y, x, dir):
+    self.x = x
+    self.y = y
+    self.dir = dir
+
+  def move(self):
+    (dy, dx) = STORM[self.dir]
+    # check for wall at new spot and restart
+
+  def mark(self, b):
+    v = b.b[self.y][self.x]
+    if v in STORM.keys():
+      n = "2"
+    if v != SPACE:
+      n = str(int(v) + 1)
+    else:
+      n = self.dir
+    b.b[self.y][self.x] = n
+
 class Valley:
   def __init__(self, path=None):
     self.moves = [
@@ -26,14 +46,13 @@ class Valley:
         for x, mark in enumerate(list(line.strip())):
           st = STORM.get(mark, False)
           if st:
-            self.storms.append([(y,x), mark])
+            self.storms.append(Storm(y, x, mark))
         self.board.append(list(line.strip()))
         y += 1
 
   def move_storms(self):
     for s in self.storms:
-      (dy, dx) = STORM[s[1]]
-      # if new spot is a wall, move to the other side
+      s.move()
 
   def find_path(self):
     # put start on queue
