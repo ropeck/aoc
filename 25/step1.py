42628a53c69d4ae9dbd7de21140ef62b6d950c91
Rodney Peck <ropeck@google.com>
2022-12-24 23:20:43 -0800

    to snafu

--- 25/step1.py
+++ 25/step1.py
@@ -9,6 +9,37 @@ def numberToBase(n, b):
         n //= b
     return digits[::-1]
 
+def carry(n, i):
+  if i < len(n)-1:
+    n[i+1] = (n[i+1] + 1) 
+    if n[i+1] > 4:
+      n[i+1] = n[i+1] % 5
+      n = carry(n, i+1)
+  else:
+    n.append(1)
+  return n
+
+def toSnafu(d):
+  n = numberToBase(d, 5)
+  n1 = n.copy()
+  n.reverse()
+  for i, v in enumerate(n):
+    if v == 4:
+      if i < len(n)-1:
+        n = carry(n, i)
+      else:
+        n.append(1)
+      n[i] = "-"
+    if v == 3:
+      if i < len(n)-1:
+        n = carry(n, i)
+      else:
+        n.append(1)
+      n[i] = "="
+  n.reverse()
+  conv = "".join([str(i) for i in n])
+  return conv
+
 def main(path):
   total = 0
   with open(path, "r") as fh:
@@ -16,26 +47,11 @@ def main(path):
     line = True
     while line:
       line = fh.readline().strip()
+      if not line:
+        break
       d = line.split()
-      print(d)
-      n = numberToBase(int(d[1]), 5)
-      print(n)
-      n.reverse()
-      for i, v in enumerate(n):
-        if v == 4:
-          if i < len(n)-1:
-            n[i] += 1
-          else:
-            n.append(1)
-          n[i] = "-"
-        if v == 3:
-          if i < len(n)-1:
-            n[i] += 1
-          else:
-            n.append(1)
-          n[i] = "="
-      n.reverse()
-      print("".join([str(i) for i in n]))
+      conv = toSnafu(int(d[1]))
+      print (d, conv)
       print("")
 
   #print(f'total: {total}')
