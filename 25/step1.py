8445f3977015a09189c0ae7f65d7b00ce5fcac2a
Rodney Peck <ropeck@google.com>
2022-12-24 22:58:39 -0800

    checkin

--- 25/step1.py
+++ 25/step1.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
 import sys
+def numberToBase(n, b):
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    return digits[::-1]
 
 def main(path):
   total = 0
-  # with open(path, "r") as fh:
-  #   line = fh.readline().strip()
-  #   while True:
+  with open(path, "r") as fh:
+    header = fh.readline()
+    line = True
+    while line:
+      line = fh.readline().strip()
+      d = line.split()
+      print(d)
+      n = numberToBase(int(d[1]), 5)
+      print(n)
+      n.reverse()
+      for i, v in enumerate(n):
+        if v == 4:
+          if i < len(n)-1:
+            n[i] += 1
+          else:
+            n.append(1)
+          n[i] = "-"
+        if v == 3:
+          if i < len(n)-1:
+            n[i] += 1
+          else:
+            n.append(1)
+          n[i] = "="
+      n.reverse()
+      print("".join([str(i) for i in n]))
+      print("")
 
-  for line in open(path,"r"):
-    pass
-  print(f'total: {total}')
+  #print(f'total: {total}')
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
