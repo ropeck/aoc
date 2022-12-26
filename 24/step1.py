429c6f5f07b79579e704595245d64ec3c922dc5c
Rodney Peck <ropeck@google.com>
2022-12-26 14:31:31 -0800

    removing board array

--- 24/step1.py
+++ 24/step1.py
@@ -8,31 +8,52 @@ STORM = {"<": (0,-1), ">": (0,1), "^": (-1,0), "v": (1,0)}
 DIR = STORM
 
 class Storm:
-  def __init__(self, x, y, dir):
+  def __init__(self, valley, x, y, dir):
     self.x_init = x
+    self.x = x
     self.y_init = y
+    self.y = y
     self.dir = dir
+    self.valley = valley
+
+  def __repr__(self):
+    return f'<Storm ({self.x_init},{self.y_init}) {self.dir}>'
+  def width(self):
+    return self.valley.width
+  def height(self):
+    return self.valley.height
 
   def move(self, t):
-    self.x = (self.x_init + t) % self.width
-    self.y = (self.y_init + t) % self.height
+    (dy, dx) = DIR[self.dir]
+    # adjust to edges here..
+    self.x = 1 + (self.x_init + t*dx - 1) % (self.width()-2)
+    self.y = 1 + (self.y_init + t*dy - 1) % (self.height()-2)
+    return self.position()
+  def position(self):
+    return (self.x, self.y)
 
 
 class Valley:
   def __init__(self, path):
     self.board = []
     self.storms = []
-    y = 0
+    h = 0
     with open(path, "r") as fh:
-      for line in fh:
-        for x, mark in enumerate(list(line.strip())):
-          st = STORM.get(mark, False)
-          if st:
-            self.storms.append(Storm(self, x, y, mark))
-        self.board.append(list(line.strip()))
-        y += 1
+      self.board = fh.read().splitlines()
+      for line in self.board:
+        row = list(line.strip())
+        for x, mark in enumerate(row):
+          if mark in STORM.keys():
+            self.storms.append(Storm(self, x, h, mark))
+        h += 1
     self.height = len(self.board)
     self.width = len(self.board[0])
+    for x, v in enumerate(self.board[0]):
+      if v == SPACE:
+        self.start = (x, 0)
+    for x, v in enumerate(self.board[-1]):
+      if v == SPACE:
+        self.finish = (x, self.height-1)
 
   def draw(self, coord=None, mark=None):
     b = [r.copy() for r in self.board]
@@ -44,59 +65,45 @@ class Valley:
     print("")
 
   def find_path(self):
-    t = 1
-    path = []
-    queue = deque()
-    for x, v in enumerate(self.board[0]):
-      if self.board[0][x] == SPACE:
-        queue.append((0,x))
-    print("initial state")
-    self.draw((0,x), "E")
-    move_dir = ""
-    prev = None
+    found=[]
+    queue = deque([(1, self.start, []),])
     while queue:
-      self.move_storms()
-      ey, ex = queue.pop()
-      if (ey, ex) not in path:
-        path.append((ey, ex))
+      print(len(queue))
+
+      (t, (x, y), p) = queue.popleft()
+      if t > 18:
+        break
+      orig_p = p.copy()
+      p.append((x,y))
+      for s in self.storms:
+        s.move(t)
+      b=[s.position() for s in self.storms]
+
       for dir, (dy, dx) in DIR.items():
-        ny = (ey + dy) % self.height
-        nx = (ex + dx) % self.width
-        if self.board[ny][nx] == SPACE and (ny, nx) not in path:
-          queue.append((ny,nx))
-          move_dir = dir
-          break
-      val = self.board[ey][ex]
-      if move_dir == "wait" and len(queue) == 0:
-        queue.append(prev)
-        (ny, nx) = prev
-        move_dir = None
-      elif not queue:
-        move_dir = "wait"
-        queue.append((ey, ex))
-      prev = (ey, ex)
-      # loop around position looking for open spaces, put them on the queue
-      # save current location to path
-      # choose the move and mark it
-      print(queue)
-      print("Minute", t, "move", move_dir)
-      self.draw((ny,nx), "E")
-      t += 1
-      if t>30:
-        break
-      if ey == len(self.board):
-        break
-      # nx, ny = pop_move
-      # find possible moves, push them onto the queue to check. save storms too?
-      # for each move:
-      #   at exit?:
-      #      add path score
-   # sort the paths by score
-   # return best path
+        new_loc = (x + dx, y + dy)
+        if new_loc == self.finish:
+          found.append((t, p.copy()))
+        if ((x + dx <= 0 or x + dx >= self.width-1) or
+            (y + dy <= 0 or y + dy >= self.height-1)):
+          continue
+        if self.board[y+dy][x+dx] == WALL:
+          continue
+        if new_loc not in b + orig_p:
+          queue.append((t + 1, new_loc, p.copy()))
+      if (x,y) not in b and y != 0:
+        queue.append((t+1, (x, y), p.copy()))
+
+
+
+    return found
 
 def main(path):
   v = Valley(path)
-  return v.find_path()
+  res = v.find_path()
+  print(v.finish)
+  print("len", len(res))
+  for l in res[:10]:
+    print(l)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
