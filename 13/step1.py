08f1a94a134fea9a107685268072120ecffa82af
Rodney Peck <ropeck@google.com>
2022-12-28 12:35:33 -0800

    working on question 13 signals

--- 13/step1.py
+++ 13/step1.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
 import sys
 
-def main(path):
-  total = 0
-  # with open("input","r") as fh:
-  #   line = fh.readline().strip()
-  #   while True:
+def compare(l, r):
+  if type(l) == int and type(r) == int:
+    return l <= r
+  if type(l) == list and type(r) == list:
+    for a,b in zip(l, r):
+      if not compare(a, b):
+        return False
+    return len(l) <= len(r)
+  if type(l) == int:
+    return r and compare(l,r[0])
+  else:
+    return l and compare(l[0],r)
 
-  for line in open(path,"r"):
-    pass
-  print(f'total: {total}')
+def main(path):
+  signals = []
+  with open(path, "r") as fh:
+    while True:
+      a = fh.readline()
+      if not a:
+        break
+      b = fh.readline()
+      _ = fh.readline()
+      signals.append((eval(a),eval(b)))
+
+  sum = 0
+  for i, (l, r) in enumerate(signals):
+    if compare(l, r):
+      sum += (i+1)
+      print(i+1, l, r)
+
+  print("")
+  print("total", sum)
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
