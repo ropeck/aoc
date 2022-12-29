34f6c780b7fe200bdd1fe5e21ea98e832ad79bd7
Rodney Peck <ropeck@google.com>
2022-12-29 15:43:15 -0800

    part2

--- 12/test/test1.py
+++ 12/test/test1.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import unittest
 import step1
+import step2
 
-class TestStep1TestInputLength(unittest.TestCase):
+class TestStep1(unittest.TestCase):
     def runTest(self):
-        self.assertEqual(31, step1.read_drawing("test/input"))
+        self.assertEqual(31, step1.main("test/test_input"))
+
+class TestStep2(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(29, step2.main("test/test_input"))
