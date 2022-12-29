77fb6f71f515d0a890f182feef2bc9c66a849820
Rodney Peck <ropeck@google.com>
2022-12-28 16:12:43 -0800

    editing

--- 13/step1.py
+++ 13/step1.py
@@ -9,13 +9,15 @@ def compare(l, r, d=0):
     r = [r]
   for a,b in zip(l, r):
     if type(a) == int and type(b) == int:
+      print(" " * (d+2), a, b, b-a)
       rec = b - a
     else:
-      rec = compare(a, b)
-    print("rec", rec)
+      rec = compare(a, b, d+2)
+    print(" "*d + "rec", rec)
     if rec:
-      return rec > 0
-  return len(r) > len(l)
+      return rec
+  print("equal", len(r) - len(l))
+  return len(r) - len(l)
 
 def main(path):
   signals = []
@@ -30,7 +32,7 @@ def main(path):
 
   sum = 0
   for i, (l, r) in enumerate(signals):
-    if compare(l, r):
+    if compare(l, r) > 0:
       sum += (i+1)
       print("ok", i+1)
     print("")
