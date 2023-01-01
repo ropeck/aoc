ab87a7010c4190b2f6e6082b1ccf1499907c7fdb
Rodney Peck <ropeck@google.com>
2022-12-31 21:14:27 -0800

    clean up and working 3dcube part1

--- 22/step2.py
+++ 22/step2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from collections import deque
-from icecream import ic
 import re
 import sys
+from collections import deque
 
 # Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
 DIR_NUMBER = '>v<^'
@@ -12,9 +11,12 @@ HEIGHT = WIDTH = 50
 FACE_ORIGINS = [(50,0), (100,0), (50, 50), (0, 100), (50, 100), (0, 150)]
 
 class Grid:
-  def __init__(self, grid_data):
+  def __init__(self, path):
+    with open(path, "r") as fh:
+      (griddata, followdata) = fh.read().split("\n\n")
+    self.follow = deque(re.findall("(\d+|[LR])", followdata))
     self.grid_source = []
-    for line in grid_data.splitlines():
+    for line in griddata.splitlines():
         line = (line + " "*150)[:150]
         print(line+"|")
         self.grid_source.append(list(line))
@@ -25,6 +27,17 @@ class Grid:
         f.append(self.grid_source[cy+y][cx:cx+50+1])
       self.face.append(f)
     self.cur_face = 0
+
+    self.set_pos(*self.find_start())
+    self.set_dir(0)
+                   #      R       D       L       U
+    self.facemap = {1: [(2, 0), (3, 0), (2, 0), (5, 0)],
+                    2: [(1, 0), (2, 0), (1, 0), (2, 0)],
+                    3: [(3, 0), (5, 0), (3, 0), (1, 0)],
+                    4: [(5, 0), (6, 0), (5, 0), (6, 0)],
+                    5: [(4, 0), (1, 0), (4, 0), (3, 0)],
+                    6: [(6, 0), (4, 0), (6, 0), (4, 0)]
+                    }
 
 
   def get(self, x, y, f=None):
@@ -59,45 +72,61 @@ class Grid:
 
   def cubemap(self, dir):
     self.set_dir(DIR_NUMBER.index(dir))
-                #     R       D       L       U
-    facemap = {1: [(2, 0), (3, 0), (2, 0), (5, 0)],
-               2: [(1, 0), (2, 0), (1, 0), (2, 0)],
-               3: [(3, 0), (5, 0), (3, 0), (1, 0)],
-               4: [(5, 0), (6, 0), (5, 0), (6, 0)],
-               5: [(4, 0), (1, 0), (4, 0), (3, 0)],
-               6: [(6, 0), (4, 0), (6, 0), (4, 0)]
-              }
-    face, rot = facemap[self.cur_face+1][self.dir]
+    face, rot = self.facemap[self.cur_face+1][self.dir]
     return face-1, rot
 
-  def move_forward(self):
+  def move_forward(self, n=1):
     (dy, dx) = MOVEDIR[DIR_NUMBER[self.dir]]
-    ny = self.y + dy
-    nx = self.x + dx
-    updated = False
-    next_face = self.cur_face
-    if nx < 0:
-      updated = True
-      next_face, rot = self.cubemap('<')
-    elif nx >= WIDTH:
-      updated = True
-      next_face, rot = self.cubemap('>')
-    if ny < 0:
-      updated = True
-      next_face, rot = self.cubemap('^')
-    elif ny >= HEIGHT:
-      updated = True
-      next_face, rot = self.cubemap('v')
-    if updated:
-      ny %= WIDTH
-      nx %= HEIGHT
-    if self.get(nx, ny, next_face) == "#":
-      print(f'wall {self.get_pos()}')
-      return False
-    self.x = nx
-    self.y = ny
-    self.cur_face = next_face
+    for i in range(n):
+      ny = self.y + dy
+      nx = self.x + dx
+      updated = False
+      next_face = self.cur_face
+      if nx < 0:
+        updated = True
+        next_face, rot = self.cubemap('<')
+      elif nx >= WIDTH:
+        updated = True
+        next_face, rot = self.cubemap('>')
+      if ny < 0:
+        updated = True
+        next_face, rot = self.cubemap('^')
+      elif ny >= HEIGHT:
+        updated = True
+        next_face, rot = self.cubemap('v')
+      if updated:
+        ny %= WIDTH
+        nx %= HEIGHT
+      if self.get(nx, ny, next_face) == "#":
+        print(f'wall {self.get_pos()}')
+        return False
+      self.x = nx
+      self.y = ny
+      self.cur_face = next_face
     return True
+
+  def process_follow(self):
+    follow = self.follow.copy()
+    s=0
+    while follow:
+      move = follow.popleft()
+      s += 1
+      print(f'[{s}]')
+      x, y = self.get_pos()
+      print(f'{x},{y} move {move}')
+      if move in ["L","R"]:
+        self.set_dir((self.dir + TURN[move]) % len(DIR_NUMBER))
+        print(f'turn {move} dir {DIR_NUMBER[self.dir]} {MOVEDIR[DIR_NUMBER[self.dir]]}')
+        continue
+      #print(f'grid[{x},{y}]={grid[ny][nx]}')
+      else:
+        self.move_forward(int(move))
+
+    print(x, y, DIR_NUMBER[self.dir])
+    #  The final password is the sum of 1000 times the row, 4 times the column, and the facing.
+    pw = (y+1)*1000 + (x+1)*4 + self.dir
+    print(f'part1 password: {pw}')
+    return pw
 
 def draw(b, y=None):
   b = [["{:3d}".format(i)] + s for i,s in enumerate(b)]
@@ -107,37 +136,28 @@ def draw(b, y=None):
     print("".join(l).rstrip())
   print("")
 
+class Grid3d(Grid):
+
+  def __init__(self, data):
+    super(Grid3d, self).__init__(data)
+                   #      R       D       L       U
+    self.facemap = {1: [(2, 0), (3, 0), (2, 0), (5, 0)],
+                    2: [(1, 0), (2, 0), (1, 0), (2, 0)],
+                    3: [(3, 0), (5, 0), (3, 0), (1, 0)],
+                    4: [(5, 0), (6, 0), (5, 0), (6, 0)],
+                    5: [(4, 0), (1, 0), (4, 0), (3, 0)],
+                    6: [(6, 0), (4, 0), (6, 0), (4, 0)]
+                    }
+
 def main(path):
-  with open(path, "r") as fh:
-    (griddata, followdata) = fh.read().split("\n\n")
-  grid = Grid(griddata)
-  follow = deque(re.findall("(\d+|[LR])", followdata))
-  s = 0
+  grid = Grid(path)
+  pw = grid.process_follow()
+  print(f'part1 password: {pw}')
 
-  grid.set_pos(*grid.find_start())
-  grid.set_dir(0)
+  grid3d = Grid3d(path)
+  pw = grid3d.process_follow()
+  print(f'part2 password: {pw}')
 
-  while follow:
-    move = follow.popleft()
-    s += 1
-    print(f'[{s}]')
-    x, y = grid.get_pos()
-    print(f'{x},{y} move {move}')
-    if move in ["L","R"]:
-      grid.set_dir((grid.dir + TURN[move]) % len(DIR_NUMBER))
-      print(f'turn {move} grid.dir {DIR_NUMBER[grid.dir]} {MOVEDIR[DIR_NUMBER[grid.dir]]}')
-      continue
-    #print(f'grid[{x},{y}]={grid[ny][nx]}')
-
-    for n in range(int(move)):
-      if not grid.move_forward():
-        break
-    #draw(grid, y)
-
-  print(x, y, DIR_NUMBER[grid.dir])
-  #  The final password is the sum of 1000 times the row, 4 times the column, and the facing.
-  pw = (y+1)*1000 + (x+1)*4 + grid.dir
-  print(f'password: {pw}')
   return pw
 
 
