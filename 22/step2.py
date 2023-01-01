162f9e2fe1d2ec7e1f34a3ee763c43fc2d5fd3cb
Rodney Peck <ropeck@google.com>
2022-12-31 17:14:38 -0800

    multi face cube

--- 22/step2.py
+++ 22/step2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from collections import deque
+from icecream import ic
 import re
 import sys
 
 # Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
-FACE = '>v<^'
+DIR_NUMBER = '>v<^'
 TURN={'L': -1, 'R': 1}
 MOVEDIR = {'>': (0,1), '<': (0,-1), '^': (-1,0), 'v': (1,0)}
 HEIGHT = WIDTH = 50
@@ -49,30 +50,43 @@ class Grid:
     return x, y
 
   def cubemap(self, dir):
-    facemap = {(1, 'L'): (4, 2)}
+    self.set_dir(DIR_NUMBER.index(dir))
+    facemap = {1: [(2,0), (3, 0), (5, 0), (2, 0)],
+               2: [(1,0), (2, 0), (2,0), (1, 0)],
+               3: [(3, 0), (5, 0), (1, 0), (3, 0)],
+               4: [(5, 0), (6, 0), (6, 0), (5, 0)],
+               5: [(4, 0), (1, 0), (3, 0), (4, 0)],
+               6: [(6, 0), (4, 0), (4, 0), (6, 0)]
+              }
     try:
-      face, rot = facemap[(self.cur_face-1, self.dir)]
-      return face, rot
+      face, rot = facemap[(self.cur_face+1, self.dir)]
+      return face-1, rot
     except KeyError:
       return self.cur_face, 0
 
   def move_forward(self):
-    (dy, dx) = MOVEDIR[FACE[self.dir]]
-    grid = self.face[self.cur_face]
+    (dy, dx) = MOVEDIR[DIR_NUMBER[self.dir]]
     ny = self.y + dy
     nx = self.x + dx
-    new_face = None
+    updated = False
     if nx < 0:
-      new_face, rot = self.cubemap('L')
-    elif nx > WIDTH:
-      new_face, rot = self.cubemap('R')
+      updated = True
+      new_face, rot = self.cubemap('<')
+    elif nx > WIDTH-1:
+      updated = True
+      new_face, rot = self.cubemap('>')
     if ny < 0:
-      new_face, rot = self.cubemap('D')
-    elif ny > WIDTH:
-      new_face, rot = self.cubemap('U')
-    if new_face:
+      updated = True
+      new_face, rot = self.cubemap('v')
+    elif ny > HEIGHT-1:
+      updated = True
+      new_face, rot = self.cubemap('^')
+    if updated:
       ny %= WIDTH
       nx %= HEIGHT
+      self.cur_face = new_face
+    self.x = nx
+    self.y = ny
     return nx, ny
 
 def draw(b, y=None):
@@ -97,23 +111,22 @@ def main(path):
     x, y = grid.get_pos()
     print(f'{x},{y} move {move}')
     if move in ["L","R"]:
-      grid.set_dir((grid.dir + TURN[move]) % len(FACE))
-      print(f'turn {move} grid.dir {FACE[grid.dir]} {MOVEDIR[FACE[grid.dir]]}')
+      grid.set_dir((grid.dir + TURN[move]) % len(DIR_NUMBER))
+      print(f'turn {move} grid.dir {DIR_NUMBER[grid.dir]} {MOVEDIR[DIR_NUMBER[grid.dir]]}')
       continue
     #print(f'grid[{x},{y}]={grid[ny][nx]}')
 
     for n in range(int(move)):
       nx, ny = grid.move_forward()
-      grid.set(x,y, FACE[grid.dir])
+      # grid.set(x, y, DIR_NUMBER.index(grid.dir))
+      # ic(grid.get(nx, ny), nx, ny)
       if grid.get(nx, ny) == "#":
         break
-      x = nx
-      y = ny
     #draw(grid, y)
 
-  print(x,y,FACE[dir])
+  print(x, y, DIR_NUMBER[grid.dir])
   #  The final password is the sum of 1000 times the row, 4 times the column, and the facing.
-  pw = (y+1)*1000 + (x+1)*4 + dir
+  pw = (y+1)*1000 + (x+1)*4 + grid.dir
   print(f'password: {pw}')
   return pw
 
