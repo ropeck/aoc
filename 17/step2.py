69eb6a0062d5c5add622e38aae63e4e558e0189f
Rodney Peck <ropeck@google.com>
2023-01-07 14:02:59 -0800

    save the tower height and rock number as the tower grows

--- 17/step2.py
+++ 17/step2.py
@@ -8,15 +8,15 @@ def main(path):
     with open("tower-output.new", "r") as fh:
       t = []
       for l in fh:
-        r = l.strip.split()
-        t.append((int(r[0]), r[1]))
+        r = l.strip().split()
+        t.append((int(r[0]), " ".join(r[1:])))
   except IOError:
     step1.main(path, 10_000_000)
     with open("tower-output.new", "r") as fh:
       t = []
       for l in fh:
         r = l.strip.split()
-        t.append((int(r[0]), r[1]))
+        t.append((int(r[0]), " ".join(r[1:])))
   print(f'tower is {len(t)} tall')
   match = None
   stop_loop = False
