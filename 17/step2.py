00e8dcffbf4a181e7f02a3569f4717fcdde90a0d
Rodney Peck <ropeck@google.com>
2023-01-06 23:06:57 -0800

    top match check early if the bounds overlap

--- 17/step2.py
+++ 17/step2.py
@@ -12,17 +12,29 @@ def main(path):
     with open("tower-output.new", "r") as fh:
       t = [int(l.strip()) for l in fh]
 
+  print(f'tower is {len(t)} tall')
   match = None
+  stop_loop = False
   for a in range(len(t)):
     if match and a == match[1]:
+      print("caught middle")
       break
+
     for b in range(a+1, len(t)):
+      if stop_loop:
+        stop_loop = False
+        break
       if t[a] == t[b]:
         for i in range(len(t) - b):
           if t[a + i] != t[b + i]:
             break
+          if match and a + i > match[1]:
+            # print(f'middle overlap: {a} {a+i}')
+            stop_loop = True
+            break
         if not match or (i > match[2]):
           match = (a, b, i)
+          print(match)
 
 
   print(match)
