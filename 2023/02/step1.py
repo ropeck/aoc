8f476ebfb230be97b47c7a2792f156fab7eee11c
Rodney Peck <ropeck@google.com>
2023-12-01 21:46:31 -0800

    update for day 2; with solutions

--- 2023/02/step1.py
+++ 2023/02/step1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import aocd
+import re
 import sys
 
 def main(test):
@@ -10,7 +12,24 @@ def main(test):
   else:
     data = mod.example_data
     pass
-  
+ 
+  limit = {"red": 12, "green": 13, "blue": 14} 
+
+  for line in data.splitlines():
+    failed = False
+    m = re.match(r"Game (\d+): (.*)", line)
+    num = m.group(1)
+    v = {}
+    for draw in m.group(2).split("; "):
+        for color_count in draw.split(", "):
+            (n, color) = color_count.split(" ")
+            if int(n) > limit[color]:
+                failed = True
+    if not failed:
+        total += int(num)
+  print(total)
+  if not test:
+      aocd.submit(total, part="a", day=2, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
--- 2023/02/step2.py
+++ 2023/02/step2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import aocd
+import re
 import sys
 
 def main(test):
@@ -10,7 +12,26 @@ def main(test):
   else:
     data = mod.example_data
     pass
-  
+ 
+  limit = {"red": 12, "green": 13, "blue": 14} 
+
+  for line in data.splitlines():
+    failed = False
+    m = re.match(r"Game (\d+): (.*)", line)
+    num = m.group(1)
+    v = {}
+    p = 1
+    for draw in m.group(2).split("; "):
+        for color_count in draw.split(", "):
+            (n, color) = color_count.split(" ")
+            if int(n) > v.get(color, 0):
+                v[color] = int(n)
+    for n in v.values():
+        p = p * n
+    total += p
+  print(total)
+  if not test:
+      aocd.submit(total, part="b", day=2, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
