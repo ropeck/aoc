6cdce2658f376213589fb886c6db08532c6075e8
Rodney Peck <ropeck@google.com>
2023-12-03 21:56:48 -0800

    set up for day 5

---
+++ 2023/05/step1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import aocd
+import re
+import sys
+
+_DAY = 5
+
+def main(test):
+  total = 0
+  test = 1
+  mod = aocd.models.Puzzle(year=2023, day=_DAY)
+  if not test:
+    data = mod.input_data
+  else:
+    data = mod.example_data
+    pass
+
+
+  print(total)
+  if not test:
+      aocd.submit(total, part="a", day=_DAY, year=2023)
+
+
+if __name__ == '__main__':
+  main(len(sys.argv) > 1)
