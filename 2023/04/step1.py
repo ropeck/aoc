bf7059d38009c13df750860d9b97ab92050fd71a
Rodney Peck <ropeck@google.com>
2023-12-03 21:35:19 -0800

    part1 working

--- 2023/04/step1.py
+++ 2023/04/step1.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 import aocd
+import re
 import sys
 
 def main(test):
   total = 0
-  # test = 1
+  #test = 1
   mod = aocd.models.Puzzle(year=2023, day=4)
   if not test:
     data = mod.input_data
   else:
     data = mod.example_data
     pass
-  
+
+  for card in data.splitlines():
+    m = re.match(r"Card.*:([ \d]+) \|(.*)", card)
+    if not m:
+      print("no match?", card)
+      sys.exit(1)
+    win_str, card_str = m.groups()
+    score = 0
+    winners = [int(x) for x in win_str.split()]
+    for card in [int(x) for x in card_str.split()]:
+      if card not in winners:
+          continue
+      if score:
+        score = score * 2
+      else:
+        score = 1
+    total += score
+
   print(total)
   if not test:
       aocd.submit(total, part="a", day=4, year=2023)
--- 2023/04/step2.py
+++ 2023/04/step2.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
 import aocd
+import re
 import sys
 
 def main(test):
   total = 0
-  # test = 1
+#  test = 1
   mod = aocd.models.Puzzle(year=2023, day=4)
   if not test:
     data = mod.input_data
   else:
     data = mod.example_data
     pass
-  
+
+  line = 1
+  off = {}
+  for card in data.splitlines():
+    print(card)
+    m = re.match(r"Card.*:([ \d]+) \|(.*)", card)
+    if not m:
+      print("no match?", card)
+      sys.exit(1)
+    win_str, card_str = m.groups()
+    score = 0
+    winners = [int(x) for x in win_str.split()]
+    for card in [int(x) for x in card_str.split()]:
+      if card in winners:
+        score += 1
+        off[score+line] = off.get(score+line, 0) + (1 + off.get(line, 0))
+  #      print("win", score, off[score+line], card)
+
+
+    line += 1
+
+  print(off)
   print(total)
+  total = sum(off.values()) + line - 1
+  print("sum", total)
   if not test:
-      aocd.submit(total, part="a", day=4, year=2023)
+      aocd.submit(total, part="b", day=4, year=2023)
 
 
 if __name__ == '__main__':
