282da2c970c8cc71395aa0a7bd90c55c9e64bee8
Rodney Peck <ropeck@google.com>
2023-12-04 21:37:24 -0800

    day 5

--- 2023/05/step1.py
+++ 2023/05/step1.py
@@ -5,16 +5,57 @@ import sys
 
 _DAY = 5
 
+# seeds: 79 14 55 13
+
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
+
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
+
+# water-to-light map:
+# 88 18 7
+# 18 25 70
 def main(test):
   total = 0
   test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
-    data = mod.input_data
+    data = mod.input_data.splitlines()
   else:
-    data = mod.example_data
+    data = mod.example_data.splitlines()
     pass
 
+  seeds = []
+  maps = {}
+
+  # get the seeds line
+  while not seeds:
+    line = data.pop(0)
+    m = re.match(r'seeds: (.*)', line)
+    if m:
+      seeds = [int(x) for x in m.group(1).split()]
+      _ = data.pop(0)  # skip the blank line after
+    if len(data) == 0:
+      print("seeds not found in data")
+      sys.exit(1)
+
+  # parse maps
+  line = data.pop(0)
+  while line:
+    m = re.match(r'(.*)-to-(.*) map:', line)
+    if m:
+      src, dst = m.groups()
+    
 
   print(total)
   if not test:
