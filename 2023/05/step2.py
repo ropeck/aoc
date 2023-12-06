7a787347e9eaa05441b36b6bf85f25b0d0bffb77
Rodney Peck <ropeck@google.com>
2023-12-05 18:39:05 -0800

    working on step2 in reverse lookup 'bruteforce'

--- 2023/05/step2.py
+++ 2023/05/step2.py
@@ -7,6 +7,7 @@ _DAY = 5
 
 maps = {}
 dst_map = {}
+src_map = {}
 
 def main(test):
   total = 0
@@ -38,13 +39,14 @@ def main(test):
     if m:
       src, dst = m.groups()
       dst_map[src] = dst
+      src_map[dst] = src
       r = []
       while line and data:
         line = data.pop(0)
         rr = [int(n) for n in line.split() if n]
         if rr:
           r.append(rr)
-      maps[src] = r
+      maps[dst] = r   # was src
       if data:
         line = data.pop(0)
       else:
@@ -59,31 +61,43 @@ def main(test):
   while seed_range:
     st = seed_range.pop(0)
     c = seed_range.pop(0)
-    print(f'{st} - {st + c}, {c}')
-    while c:
-      loc = get_location(st)
-      if not min_location or loc < min_location:
-        min_location = loc
-      c -= 1
+    seeds.append((st, c))
 
-  print("min loc", min_location)
+  test_seed = 0
+  while True:
+    n = reverse_map(test_seed)
+    if in_range(n, seeds):
+      min_location = n
+      break
+    else:
+      test_seed += 1
+
+  print("min loc", test_seed)
   if not test:
-      aocd.submit(min_location, part="b", day=_DAY, year=2023)
+      aocd.submit(test_seed, part="b", day=_DAY, year=2023)
 
+def in_range(n, s):
+  s = sorted(s)
+  for (st, c) in s:
+    if n >= st and n < st + c:
+      return True
+    if st > n:
+      break
+  return False
 
-def get_location(n):
-    thing = 'seed'
-    while thing != 'location':
-      dst = dst_map[thing]
-      dst_n = range_map(n, maps[thing])
-      thing = dst
-      n = dst_n
+def reverse_map(n):
+    thing = 'location'
+    while thing != 'seed':
+      src = src_map[thing]
+      src_n = range_map(n, maps[thing])
+      thing = src
+      n = src_n
     return n
 
 def range_map(n, rm_lines):
   for (dst, src, r) in rm_lines:
-    if n >= src and n < src+r:
-      return (n - src) + dst
+    if n >= dst and n < dst+r:
+      return (n - dst) + src
   return n
 
 if __name__ == '__main__':
