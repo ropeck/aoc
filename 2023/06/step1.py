c6129176eada942a6cf8e034614ef4bc6ea75463
Rodney Peck <ropeck@google.com>
2023-12-05 20:34:36 -0800

    update next day

---
+++ 2023/06/step1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import aocd
+import re
+import sys
+
+_DAY = 6
+
+def main(test):
+  total = 0
+  test = 1
+  mod = aocd.models.Puzzle(year=2023, day=_DAY)
+  if not test:
+    data = mod.input_data.splitlines()
+  else:
+    data = mod.example_data.splitlines()
+    pass
+
+  if not test:
+      aocd.submit(min(location), part="a", day=_DAY, year=2023)
+
+if __name__ == '__main__':
+  main(len(sys.argv) > 1)
