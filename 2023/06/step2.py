cd1fe2b8ec88a197c4eb39cedbbdf71f2742bf88
Rodney Peck <ropeck@google.com>
2023-12-05 21:30:43 -0800

    step1 done day6, running step2

--- 2023/06/step2.py
+++ 2023/06/step2.py
@@ -5,9 +5,16 @@ import sys
 
 _DAY = 6
 
+def travel(t):
+  ret = []
+  for wait in range(t):
+    trav = wait * (t - wait)
+    ret.append(trav)
+  return ret
+
 def main(test):
   total = 0
-  test = 1
+  # test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
@@ -15,8 +22,25 @@ def main(test):
     data = mod.example_data.splitlines()
     pass
 
+  (t, nums) = data.pop(0).split(':')
+  nums = re.sub(' ','', nums)
+  times = [int(x) for x in nums.split()]
+
+  (t, nums) = data.pop(0).split(':')
+  nums = re.sub(' ','', nums)
+  dist = [int(x) for x in nums.split()]
+
+  winners = []
+  ans = 1
+  for i in range(len(times)):
+    win = [x for x in travel(times[i]) if x > dist[i]]
+    print(i, dist[i], win)
+    winners.append(len(win))
+    ans = ans * len(win)
+  pass
+  print(winners, ans)
   if not test:
-      aocd.submit(min(location), part="b", day=_DAY, year=2023)
+      aocd.submit(ans, part="a", day=_DAY, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
