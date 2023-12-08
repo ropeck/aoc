f44ba532d43246d22a33572723aa5a4eff44674c
Rodney Peck <ropeck@google.com>
2023-12-07 21:28:15 -0800

    starting part 2

--- 2023/08/step1.py
+++ 2023/08/step1.py
@@ -2,23 +2,39 @@
 import aocd
 import re
 import sys
-from containers import Counter
 
 _DAY = 8
 
 
 
 def main(test):
-
+  # test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
 
+  directions = data.pop(0)
+  data.pop(0)
+
+  map = {}
+  for line in data:
+    m = re.match(r'(.+) = \((.+), (.+)\)', line)
+    key, l, r = m.groups()
+    map[key] = (l, r)
+  
+  count = 0
+  cur = 'AAA'
+  while cur != 'ZZZ':
+    if directions[count%len(directions)] == 'L':
+      cur = map[cur][0]
+    else:
+      cur = map[cur][1]
+    count += 1
 
   if not test:
-      aocd.submit(ans, part="a", day=_DAY, year=2023)
+      aocd.submit(count, part="a", day=_DAY, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
