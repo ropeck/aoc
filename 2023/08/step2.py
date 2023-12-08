13dfab0ded7b0319fd2fdb2a46df12a1d55db58d
Rodney Peck <ropeck@google.com>
2023-12-07 21:28:15 -0800

    starting part 2

--- 2023/08/step2.py
+++ 2023/08/step2.py
@@ -32,9 +32,29 @@ def main(test):
     else:
       cur = map[cur][1]
     count += 1
+  directions = data.pop(0)
+  data.pop(0)
+
+  map = {}
+  for line in data:
+    m = re.match(r'(.+) = \((.+), (.+)\)', line)
+    key, l, r = m.groups()
+    map[key] = (l, r)
+  
+  count = 0
+  cur = [x for x in map.keys() if x[2] == 'A']
+  while [x for x in cur if x[2] != 'Z']:
+    for n in range(len(cur)):
+      if directions[count%len(directions)] == 'L':
+        cur[n] = map[cur[n]][0]
+      else:
+        cur[n] = map[cur[n]][1]
+    if count % 1000000 == 0:
+      print ("state", count, [x for x in cur if x[2] != 'Z'])
+    count += 1
 
   if not test:
-      aocd.submit(count, part="a", day=_DAY, year=2023)
+      aocd.submit(count, part="b", day=_DAY, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
