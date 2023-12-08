9109708c01ec97a5f1e0e01aa4674b8069b87d2e
Rodney Peck <ropeck@google.com>
2023-12-07 18:37:52 -0800

    setup for day 8

---
+++ 2023/08/step1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import aocd
+import re
+import sys
+from containers import Counter
+
+_DAY = 8
+
+
+
+def main(test):
+
+  mod = aocd.models.Puzzle(year=2023, day=_DAY)
+  if not test:
+    data = mod.input_data.splitlines()
+  else:
+    data = mod.example_data.splitlines()
+
+
+  if not test:
+      aocd.submit(ans, part="a", day=_DAY, year=2023)
+
+if __name__ == '__main__':
+  main(len(sys.argv) > 1)
