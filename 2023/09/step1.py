92e668c2e3ac6f8d43b2bb4cb392f7e3da976234
Rodney Peck <ropeck@google.com>
2023-12-08 21:30:38 -0800

    part 1 day 9 complete

--- 2023/09/step1.py
+++ 2023/09/step1.py
@@ -2,6 +2,7 @@
 import aocd
 import re
 import sys
+import numpy
 
 _DAY = 9
 
@@ -15,6 +16,25 @@ def main(test):
   else:
     data = mod.example_data.splitlines()
 
+  count = 0
+  for line in data:
+    rr = []
+    cur = [int(n) for n in line.split(" ")]
+    while [n for n in cur if n]:
+      rr.append(cur)
+      cur = list(numpy.diff(cur))
+    rr.append(cur)
+    # print(rr)
+    rows = rr.copy()
+    rows.reverse()
+    total = rows.pop(0)[-1]
+    for r in rows:
+      total += r[-1]
+    print("total", total)
+    count += total
+
+  count = int(count)
+  print("answer", count)
 
   if not test:
       aocd.submit(count, part="a", day=_DAY, year=2023)
