036def520b39cc034bcd4758375fe5521e679cfb
Rodney Peck <ropeck@google.com>
2023-12-10 11:05:29 -0800

    sort of closer?

--- 2023/10/step2.py
+++ 2023/10/step2.py
@@ -15,6 +15,7 @@ dir = {
   '|': [(0,-1), (0,1)],
 }
 
+inside = 0
 
 def main(test):
 
@@ -34,6 +35,41 @@ def main(test):
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
+
+#   data = """..........
+# .S------7.
+# .|F----7|.
+# .||OOOO||.
+# .||OOOO||.
+# .|L-7F-J|.
+# .|II||II|.
+# .L--JL--J.
+# ..........
+# """.splitlines()
+  
+#   data = """.F----7F7F7F7F-7....
+# .|F--7||||||||FJ....
+# .||.FJ||||||||L7....
+# FJL7L7LJLJ||LJ.L-7..
+# L--J.L7...LJS7F-7L7.
+# ....F-J..F7FJ|L7L7L7
+# ....L7.F7||L7|.L7L7|
+# .....|FJLJ|FJ|F7|.LJ
+# ....FJL-7.||.||||...
+# ....L---J.LJ.LJLJ...
+# """.splitlines()
+  
+#   data="""FF7FSF7F7F7F7F7F---7
+# L|LJ||||||||||||F--J
+# FL-7LJLJ||||||LJL-77
+# F--JF--7||LJLJ7F7FJ-
+# L---JF-JLJ.||-FJLJJ7
+# |F|F-JF---7F7-L7L|7|
+# |FFJF7L7F-JF7|JL---7
+# 7-L-JL7||F7|L7F-7F7|
+# L.L7LFJ|||||FJL7||LJ
+# L7JLJL-JLJLJL--JLJ.L
+# """.splitlines()
 
   start = None
   for y, line in enumerate(data):
@@ -74,43 +110,93 @@ def main(test):
 
   mid = int(len(path)/2)
   print("mid", mid)
-  inside = 0
 
   path_d = {}
   for p in path:
     path_d[p] = True
 
   def is_inside(cx,cy):
-    if (cx, cy) in path:
+    if (cx, cy) in path_d:
       return False
-    lh = 1
+    lh = 0
     for x in range(0,cx):
+      # if (x, cy) in path_d and data[cy][x] == '|':
       if (x, cy) in path_d:
         lh += 1
-    rh = 1
+    rh = 0
     for x in range(cx+1, len(line)):
+      # if (x, cy) in path_d and data[cy][x] == '|':
       if (x, cy) in path_d:
         rh += 1
-    up = 1
+    up = 0
     for y in range(cy+1, len(data)):
+      # if (cx, y) in path_d and data[y][cx] == '-':
       if (cx, y) in path_d:
         up += 1
-    dn = 1
+    dn = 0
     for y in range(0, cy):
+      # if (cx, y) in path_d and data[y][cx] == '-':
       if (cx, y) in path_d:
         dn += 1
-    return not all([lh%2, rh%2, up%2, dn%2])    
+    return all([lh%2, rh%2, up%2, dn%2])    
 
+  def fill_from_coord(x,y, seen=None):
+    if not seen:
+      seen = {}
+    global inside
+    if x < 0 or x > len(data[0])-1 or y < 0 or y > len(data)-1:
+      return seen
+    if (x,y) in path_d or (x,y) in seen:
+      return seen
+    seen[(x,y)] = 1
+    seen = fill_from_coord(x-1, y, seen)
+    seen = fill_from_coord(x+1, y, seen)
+    seen = fill_from_coord(x, y-1, seen)
+    seen = fill_from_coord(x, y+1, seen)
+    l = list(data[y])
+    l[x] = '*'
+    inside += 1
+    data[y] = ''.join(l)
+    return seen
+
+  def remove_from_coord(x,y, seen=None):
+    if x < 0 or x > len(data[0])-1 or y < 0 or y > len(data)-1:
+      return seen
+    if not seen:
+      seen = {}
+    global inside
+    if (x,y) in path_d or (x,y) in seen:
+      return seen
+    seen[(x,y)] = 1
+    seen = remove_from_coord(x-1, y, seen)
+    seen = remove_from_coord(x+1, y, seen)
+    seen = remove_from_coord(x, y-1, seen)
+    seen = remove_from_coord(x, y+1, seen)
+    l = list(data[y])
+    l[x] = ' '
+    inside -= 1
+    data[y] = ''.join(l)
+    return seen
+  
+  seen = {}
   for y, line in enumerate(data):
     l = list(line)
     for x in range(len(line)):
-      if (x,y) in path_d:
-        l[x]=' '
-      if is_inside(x,y):
-        inside += 1
-        l[x]='*'
-    print(''.join(l))
+      seen = fill_from_coord(x,y, seen)
         
+  print('\n'.join(data))
+  print("inside", inside)
+
+  # remove edges
+  seen = {}
+  for y, line in enumerate(data):
+    seen = remove_from_coord(0,y, seen)
+    seen = remove_from_coord(len(line)-1,y, seen)
+  for x in range(len(line)):
+    seen = remove_from_coord(x,0, seen)  
+    seen = remove_from_coord(x,len(data)-1, seen)
+
+  print('\n'.join(data))
   print("inside", inside)
 
   if not test:
