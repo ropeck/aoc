2fd3851d9674dc8426338aa24a3ed23e441d19a7
Rodney Peck <ropeck@google.com>
2023-12-13 21:26:37 -0800

    adding today

---
+++ 2023/15/step2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import aocd
+import re
+import sys
+
+_DAY = 14
+
+=bytearray(
+        "OOOO.#.O..", "utf-8")
+def main(test):
+
+  test = 1
+  mod = aocd.models.Puzzle(year=2023, day=_DAY)
+  if not test:
+    data = mod.input_data.splitlines()
+  else:
+    data = mod.example_data.splitlines()
+
+  print("total", total)
+  # if not test:
+  #     aocd.submit(total, part="b", day=_DAY, year=2023)
+
+if __name__ == '__main__':
+  main(len(sys.argv) > 1)
