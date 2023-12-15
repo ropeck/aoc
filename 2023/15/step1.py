8348a483dfd4f5b472a920d7b922b6701abc8987
Rodney Peck <ropeck@google.com>
2023-12-14 21:16:10 -0800

    part1 works. sleep now.

--- 2023/15/step1.py
+++ 2023/15/step1.py
@@ -5,18 +5,30 @@ import sys
 
 _DAY = 15
 
+def hash(str):
+  cur = 0
+  for i, c in enumerate(str):
+    cur += ord(c)
+    cur = cur * 17
+    cur = cur % 256
+  return cur
+
 def main(test):
 
-  test = 1
+  # test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
 
+  total = 0
+  for w in data[0].split(","):
+    print (w, hash(w))
+    total += hash(w)
   print("total", total)
-  # if not test:
-  #     aocd.submit(total, part="a", day=_DAY, year=2023)
+  if not test:
+      aocd.submit(total, part="a", day=_DAY, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
