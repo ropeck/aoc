02376b2e24cfe4abd50e6a437e47e3a13140ff4b
Rodney Peck <ropeck@google.com>
2023-12-15 22:31:55 -0800

    testing

--- 2023/16/step1.py
+++ 2023/16/step1.py
@@ -4,11 +4,29 @@ import re
 import sys
 
 _DAY = 16
+beams = [(0,0,1,0)]
 
 def main(test):
+  global beams 
 
   def in_bounds(x, y):
     return x >= 0 and x < width and y >= 0 and y < height
+  
+  def draw():
+    for row in active:
+      s = []
+      for v in row:
+        if v:
+          s.append("#")
+        else:
+          s.append(" ")
+      print(''.join(s))
+
+  def add_beam(x, y, dx, dy):
+    global beams
+    if (x, y, dx, dy) not in seen and (x, y, dx, dy) not in beams:
+      beams.append((x, y, dx, dy))
+    return beams
   
   test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
@@ -20,15 +38,18 @@ def main(test):
   width = len(data[0])
   height = len(data)
   active = [[0 for x in range(width)] for y in range(height)]
-  beams = [(0,0,1,0)]
   seen = {}
 
-  while beams:
+  count = 0
+  while beams and count < 1000:
+    count += 1
     x, y, dx, dy = beams.pop(0)
     while in_bounds(x,y):
+      draw()
+      print("    ")
       print(f"{(x,y)} {beams}")
-      if seen.get((x,y), False):
-        break
+      # if seen.get((x,y), False):
+      #   break
       seen[(x,y)] = True
       active[y][x] = 1
       ch = data[y][x]
@@ -48,16 +69,19 @@ def main(test):
         
       elif ch == '|':
         if dy == 0:
-          beams.extend([(x, y-1, 0, -1), (x, y+1, 0, 1)])
+          beams = add_beam(x, y-1, 0, -1)
+          beams = add_beam(x, y+1, 0, 1)
           break
       elif ch == '-':
         if dx == 0:
-          beams.extend([(x-1, y, -1, 0), (x+1, y, 1, 0)])
+          beams = add_beam(x-1, y, -1, 0)
+          beams = add_beam(x+1, y, 1, 0)
           break
       x += dx
       y += dy
 
       
+  draw()
 
   total = 0
   for row in active:
