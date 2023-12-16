3384176852f5be06e2e6dee0fcb7ea2239a2ba6b
Rodney Peck <ropeck@google.com>
2023-12-15 21:09:58 -0800

    start

--- 2023/17/step1.py
+++ 2023/17/step1.py
@@ -5,6 +5,13 @@ import sys
 
 _DAY = 16
 
+class Beam:
+  def __init__(self, x, y, dx, dy):
+      self.x = x
+      self.y = y
+      self.dx = dx
+      self.dy = dy
+
 def main(test):
 
   test = 1
@@ -13,6 +20,13 @@ def main(test):
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
+
+  width = len(data[0])
+  height = len(data)
+  beams = [Beam(0,0)]
+  
+
+  active = [[False for x in range(width)] for y in range(height)]
 
   print("total", total)
   # if not test:
