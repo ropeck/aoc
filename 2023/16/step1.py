eaae8d0501ed3315b3d25c71aad7223eeae67d5b
Rodney Peck <ropeck@google.com>
2023-12-15 22:05:18 -0800

    save

--- 2023/16/step1.py
+++ 2023/16/step1.py
@@ -5,15 +5,11 @@ import sys
 
 _DAY = 16
 
-class Beam:
-  def __init__(self, x, y, dx, dy):
-      self.x = x
-      self.y = y
-      self.dx = dx
-      self.dy = dy
-
 def main(test):
 
+  def in_bounds(x, y):
+    return x >= 0 and x < width and y >= 0 and y < height
+  
   test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
@@ -23,8 +19,49 @@ def main(test):
 
   width = len(data[0])
   height = len(data)
-  beams = [Beam(0,0,1,0)]
-  
+  active = [[0 for x in range(width)] for y in range(height)]
+  beams = [(0,0,1,0)]
+  seen = {}
+
+  while beams:
+    x, y, dx, dy = beams.pop(0)
+    while in_bounds(x,y):
+      print(f"{(x,y)} {beams}")
+      if seen.get((x,y), False):
+        break
+      seen[(x,y)] = True
+      active[y][x] = 1
+      ch = data[y][x]
+      if ch == '.':
+        pass
+      elif ch == '/':
+        (dx, dy) = {(-1,0): (0,1),
+                    (1,0): (0, -1),
+                    (0,-1): (1, 0),
+                    (0,1): (-1, 0)}[(dx, dy)]
+        
+      elif ch == '\\':
+        (dx, dy) = {(-1,0): (0,-1),
+                    (1,0): (0, 1),
+                    (0,-1): (-1, 0),
+                    (0,1): (1, 0)}[(dx, dy)]
+        
+      elif ch == '|':
+        if dy == 0:
+          beams.extend([(x, y-1, 0, -1), (x, y+1, 0, 1)])
+          break
+      elif ch == '-':
+        if dx == 0:
+          beams.extend([(x-1, y, -1, 0), (x+1, y, 1, 0)])
+          break
+      x += dx
+      y += dy
+
+      
+
+  total = 0
+  for row in active:
+    total += sum(row)
 
   active = [[False for x in range(width)] for y in range(height)]
 
