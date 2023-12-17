ad0cf4d9c589456779c320261f02eb4f4c3ac6ce
Rodney Peck <ropeck@google.com>
2023-12-16 22:33:46 -0800

    maze

--- 2023/17/step1.py
+++ 2023/17/step1.py
@@ -5,13 +5,24 @@ import sys
 
 _DAY = 17
 
-class Beam:
+class Move:
   def __init__(self, x, y, dx, dy):
-      self.x = x
-      self.y = y
-      self.dx = dx
-      self.dy = dy
-
+    self.x, self.y = x, y
+    self.dx, self.dy = dx, dy
+    self.count = 3
+  def __repr__(self):
+    return f"<Move {self.count} ({self.x},{self.y}) {self.dx},{self.dy}>"
+  def position(self):
+    return (self.x, self.y)
+  def left(self):
+    nx, ny = {(1,0):(0,-1), (-1,0):(0,1),
+              (0,1):(-1,0), (0,-1):(1,0)}[(self.dx, self.dy)]
+    return Move(self.dx+nx, self.dy+ny, nx, ny)
+  def right(self):
+    nx, ny = {(1,0):(0, 1), (-1,0):(0,-1),
+              (0,1):(1,0), (0,-1):(-1,0)}[(self.dx, self.dy)]
+    return Move(self.dx+nx, self.dy+ny, nx, ny)
+  
 def main(test):
 
   test = 1
@@ -23,11 +34,40 @@ def main(test):
   
   d = [[int(c) for c in list(row)] for row in data]
   p = []
-  s = {}
+  seen = {}
   x, y = 0, 0
   w = len(d[0])
   h = len(d)
-  
+  cur = Move(0,0,1,0)
+  q = [(cur, [])]
+  found = False
+  while q and not found:
+    print(q)
+    print(p)
+    cur, p = q.pop(0)
+    if cur.x < 0 or cur.x > w or cur.y < 0 or cur.y > h:
+      continue
+    seen[cur] = True
+    p.append(cur.position())
+    #left
+      #depending on which direction, add left offset
+    q.append((cur.left(), p+[cur.position()]))
+    #right
+    q.append((cur.right(), p+[cur.position()]))
+    # append if not seen already
+    while cur.count != 0:
+      cur.count -= 1
+      if cur.position() in seen: continue
+      p.append(cur.position())
+      seen[cur] = True
+      cur.x += cur.dx
+      cur.y += cur.dy
+      if cur.x < 0 or cur.x > w or cur.y < 0 or cur.y > h:
+        break
+      if cur == (w,h):
+        Found = True
+        break
+
   print(d)
 
 
