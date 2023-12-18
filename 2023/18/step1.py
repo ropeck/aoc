1ed32f4652e82ad695e46f5872cef1d924d0726c
Rodney Peck <ropeck@google.com>
2023-12-18 14:31:40 -0800

    part1 working!

--- 2023/18/step1.py
+++ 2023/18/step1.py
@@ -10,6 +10,8 @@ class Lagoon:
     self.steps = data
     x = 0
     y = 0
+    orig_x = x
+    orig_y = y
     self.width = 0
     self.height = 0
     for line in data:
@@ -19,18 +21,29 @@ class Lagoon:
         x += n
       if dir == "L":
         x -= n
-      if dir == "U":
-        y -= n
       if dir == "D":
+        y -= n
+      if dir == "U":
         y += n
+      if x < 0:
+        self.width -= x
+        orig_x -= x
+        x = 0
       self.width = max([self.width, x])
+      if y < 0:
+        self.height -= y
+        orig_y -= y
+        y = 0
       self.height = max([self.height, y])
+
+    if x < 0 or y < 0:
+      raise ValueError
 
     self.d = [["." for x in range(self.width+1)]
               for y in range(self.height+1)]
 
-    x = 0
-    y = 0
+    x = orig_x
+    y = orig_y
     for line in data:
       dir, lenstr, color = line.split(" ")
       n = int(lenstr)
@@ -39,11 +52,13 @@ class Lagoon:
           x += 1
         if dir == "L":
           x -= 1
-        if dir == "U":
-          y -= 1
         if dir == "D":
+          y -= 1
+        if dir == "U":
           y += 1
         self.d[y][x] = "#"
+      #  self.draw()
+      #  print("")
         n -= 1
   def draw(self, data=None):
     if not data:
@@ -57,6 +72,7 @@ class Lagoon:
     mid = [["."] + line + ["."] for line in self.d]
     box = frame + mid + frame
     print (f"{len(box[0])}x{len(box)}")
+    self.draw(box)
     # fill from the edge
     x = 0
     y = 0
@@ -67,7 +83,7 @@ class Lagoon:
       x, y = cur
       # print(cur, q)
       s[cur] = True
-      box[y][x] = "*"
+      box[y][x] = " "
       for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
         nx = x + dx
         ny = y + dy
@@ -86,7 +102,7 @@ class Lagoon:
 
 
 def main(test):
-  test = 1
+  #test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
@@ -94,11 +110,11 @@ def main(test):
     data = mod.example_data.splitlines()
 
   lag = Lagoon(data)
-  # lag.draw()
+  #lag.draw()
   total = lag.count()
   print("total", total)
-  # if not test:
-  #     aocd.submit(total, part="a", day=_DAY, year=2023)
+  if not test:
+    aocd.submit(total, part="a", day=_DAY, year=2023)
 
 if __name__ == '__main__':
   main(len(sys.argv) > 1)
