686cf4e2247f01ae6b32703c3a40885991473af7
Rodney Peck <ropeck@google.com>
2023-12-19 20:17:11 -0800

    part 1 is solved

--- 2023/19/step1.py
+++ 2023/19/step1.py
@@ -7,11 +7,28 @@ _DAY = 19
 
 class Workflow:
   def __init__(self, str):
+    self.str = str
     m = re.match(r"(.*){(.*)}", str)
     if not m:
       raise ValueError
     self.name, rules = m.groups()
     self.rules = [Condition(x) for x in rules.split(",")]
+  
+  def apply(self, part):
+    pass
+    for r in self.rules:
+      if r.op is None:
+        return r.target
+      v = part.rating[r.var]
+      if r.op == "<":
+        if v < r.n:
+          return r.target
+      if r.op == ">":
+        if v > r.n:
+          return r.target
+    raise ValueError
+      
+
 
 class Condition:
   def __init__(self, str):
@@ -26,23 +43,51 @@ class Condition:
   def __repr__(self):
     return f"<{self.str}>"
 
+class Part:
+  def __init__(self, str):
+    m = re.match(r"{(.*)}", str)
+    r = {}
+    for a in m.group(1).split(","):
+      k,v = a.split("=")
+      r[k] = int(v)
+    self.rating = r
+
+  def sum(self):
+    return sum(self.rating.values())
+    
 
 def main(test):
-  test = 1
+  # test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
-  workflows = []
+  workflows = {}
   while True:
     line = data.pop(0)
     if not line:
       break
-    workflows.append(Workflow(line))
+    w = Workflow(line)
+    workflows[w.name] = w
   
+  total = 0
+  while data:
+    line = data.pop(0)
+    p = Part(line)
 
+    wf = 'in'
+    s = [wf]
+    while wf not in ['R', 'A']:
+      w = workflows[wf]
+      wf = w.apply(p)
+      s.append(wf)
 
+    print(" -> ".join(s), p.sum())
+    if wf == 'A':
+      total += p.sum()
+    
+  
   print("total", total)
   if not test:
     aocd.submit(total, part="a", day=_DAY, year=2023)
