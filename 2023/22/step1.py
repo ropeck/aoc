140ca3ef065e7235b156a5a425bad4eb50b26297
Rodney Peck <ropeck@google.com>
2023-12-22 12:29:13 -0800

    dropping bricks

--- 2023/22/step1.py
+++ 2023/22/step1.py
@@ -5,55 +5,62 @@ import sys
 
 _DAY = 22
 
-def draw(data):
-  print("----")
-  print("\n".join(data))
+class Brick:
+
+  def __init__(self, str=None):
+    #  '1,0,1~1,2,1'
+    self.str = str
+    self.d = []
+    if str:
+      for st in str.split("~"):
+        e = [int(x) for x in st.split(",")]
+        self.d.append(tuple(e))
+
+  def __repr__(self):
+    s = " - ".join([str(i) for i in self.d])
+    return f"<{s}>"
+
+  def val(self, n):
+    return [d[n] for d in self.d]
+  
+  def val_overlap(self, other, n):
+    return (min(self.val(n)) >= max(other.val(n)) and
+            max(self.val(n)) <= min(other.val(n)))
+    
+  def key(self):
+    return self.min_z()
+
+  def min_z(self):
+    return min(self.val(2))  # Z
+  
+  def move_z(self, dz=-1):
+    nd = []
+    for x, y, z in self.d:
+      nd.append((x, y, z+dz))
+    self.d = nd
+
+  def drop(self):
+    if self.min_z() == 1:
+      return False
+    self.move_z(-1)
+    return True
 
 def main(test):
-  # test = 1
+  test = 1
   mod = aocd.models.Puzzle(year=2023, day=_DAY)
   if not test:
     data = mod.input_data.splitlines()
   else:
     data = mod.example_data.splitlines()
 
-  height = len(data)
-  width = len(data[0])
+  d = [Brick(line) for line in data]
+  d = sorted(d, key=lambda x: x.key())
 
-  # find start
-  for y, line in enumerate(data):
-    x = line.find("S")
-    if x >= 0:
-      break
-  if not x:
-    raise ValueError
-  
-  print("start", (x, y))
-
-  # while the queue is not empty, reduce count and add neighbors
-  s = {}
-  q = [(x,y, 64)]
-  p = []
-  while q:
-    x, y, c = q.pop(0)
-    if c == 0:
-      p.append((x,y))
-      continue
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-      nx = x + dx
-      ny = y + dy
-      if nx < 0 or nx >= width or ny < 0 or ny >= height:
-        continue
-      if data[ny][nx] not in "S.":
-        continue
-      if c and (nx, ny, c - 1) not in q:
-        q.append((nx, ny, c - 1))
-  
-
-
-
-
-
+  for br in d:
+    print(br)
+    while br.drop():
+      print(br)
+    print("---")
 
   if not test:
     aocd.submit(len(p), part="a", day=_DAY, year=2023)
